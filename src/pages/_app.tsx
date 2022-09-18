4a30baefcad22d4540efd374675e076c8a1d37fe
Max Rose <max.rose366@gmail.com>
2022-09-18 17:22:55 +0100

    added ackee

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,11 @@ import KeyboardInput from "@components/KeyboardInput";
 import { githubLink, KeyboardContextType, linkedinLink } from "@constants/*";
 import useKeybindings from "@hooks/useKeybindings";
 import '@styles/globals.css'
+import { AckeeInstance } from "ackee-tracker";
 import type { AppProps } from 'next/app'
-import React, { createContext, useState } from "react";
+import { Router } from "next/router";
+import React, { createContext, useEffect, useState } from "react";
+import * as ackeeTracker from 'ackee-tracker'
 
 export const KeyboardContext = createContext<KeyboardContextType>({ add: (_, __) => false, remove: (_) => false});
 
@@ -18,6 +21,20 @@ const GlobalKeybindings: React.FC = () => {
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [bindings, setBindings] = useState(new Map<string, () => void>());
+
+  useEffect(() => {
+      if(typeof window !== "undefined") {
+          const w = window as {ackeeTrackerInstance? : AckeeInstance}
+
+          if(!w.ackeeTrackerInstance) {
+              w.ackeeTrackerInstance = ackeeTracker.create("https://ackee.max-rose.com")
+
+              w.ackeeTrackerInstance.record("e524de48-5339-4ecd-863d-870db1c004f3");
+
+              Router.events.on('routeChangeComplete', () => w.ackeeTrackerInstance?.record("e524de48-5339-4ecd-863d-870db1c004f3"))
+          }
+      }
+  }, [])
 
   const addBinding = (key: string, callback: () => void) => {
     const lowerCaseKey = key.toLowerCase();
